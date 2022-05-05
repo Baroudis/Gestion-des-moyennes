cf54b2fbca248e1ee49b818450746875e7ed29c6
Baroudis <baroudi.salloute@gmail.com>
2022-05-05 16:23:25 +0200

    finalisation des fonction de calcul et affichage

--- main.js
+++ main.js
@@ -1,60 +1,82 @@
+
 var montableau = []
 var nom = "";
-var notes = "";
+//var notes = "";
 
 
-
-
-// document.addEventListener("click", function () {
-
-//     nom = document.getElementById("nomA").value
-
-//     notes = document.getElementById("notesA").value
-
-
-
-//     console.log(notes)
-
-//     // nombre = notes.length
-//     // console.log(nombre)
-
-
-
-//     //console.log(montableau)
-
-
-// });
 
 
 function ajouterVal() {
-    // laffik = documetn.getElementById("nomA").innerHTML = 
-    nom = document.getElementById("nomA").value
-    notes = document.getElementById("notesA").value
 
+  notes = document.getElementById("notesA").value
+  montableau.push(notes)
+  // console.log(montableau)
 
-    //console.log(nom, notes)
+  montableau.forEach(function (item, index, array) {
 
-    montableau.push(nom, notes)
-
+        tousaffi = ""
     
+        if (index + 1 == 1) {
+          tousaffi += document.getElementById("demo").innerHTML = `<p>Vous avez ajouté le${index + 1}er chiffre  qui est ${item}     </p>`
+    
+          console.log(tousaffi)
+        } else {
+          document.getElementById("demo").innerHTML = `<p> ${index + 1}em :  qui est ${item}     </p>`
+        }
+        console.log(item, index + 1);
+        console.log(montableau)
+      })
+      tousaffi += tousaffi
 
-
-    console.log(montableau)
-    //montableau.forEach(element => console.log("<p>voici :",element));
-
-    // montableau.forEach(function(item, index, array) {
-    //     console.log(item, index);
-    //   });
-
-    for (var i = 0; i < montableau.length; i++) {
-        document.getElementById("loop").innerHTML = montableau.join(" ")
-        //showArray();
-        // I'm only getting "David" as output!!
-      }
-
-
+  return montableau
 
 }
 
 
 
+
+
+function afficherval() {
+
+  chaine = 0;
+
+  for (i = 0; i < montableau.length; i++) {
+    chaine += ` + ${montableau[i]}`;
+  }
+  console.log(chaine)
+
+
+
+  function sumStr(chaine) {
+    let strArr = chaine.split("+");
+    let sum = strArr.reduce(function (total, num) {
+    return parseFloat(total) + parseFloat(num);
+    });
+
+    return sum;
+
+  }
+
+  prix = sumStr(chaine)
+  largeur = montableau.length
+  total = prix / montableau.length
+  console.log(largeur)
+  console.log(prix)
+  console.log(total)
+  
+  lignes = ""
+
+para = "" 
+finall= ""
+montableau.forEach(afficherT)
+document.getElementById("loop").innerHTML = para
+
+function afficherT(item, index){
+  para += index+1 + ": " + item +"<br>"
+
+}
+if(total > 10){
+  document.getElementById("final").innerHTML = `<p style="color:green"> Bravo vous avez plus que la moyenne avec le score de ${total}`
+}else{}
+
+    }
